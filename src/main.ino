8ba91ede09529379f4dd681e7894fd531005bb26
BMaguire <benjamin.e.maguire@gmail.com>
2018-04-27 09:00:57 +1000

    added struct keyword before calls

--- src/main.ino
+++ src/main.ino
@@ -4,8 +4,8 @@ struct auth_response {
 }
 
 
-auth_response check_auth() {
-  auth_response response;
+struct auth_response check_auth() {
+  struct auth_response response;
   response.auth_result = 1; // change to 0 for false
   return response;
 }
@@ -20,7 +20,7 @@ void setup() {
 }
 
 void loop() {
-  auth_response response = check_auth();
+  struct auth_response response = check_auth();
   control(response);
 
 }
